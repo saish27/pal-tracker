941abf9df33bbdc28dcc23a49f6dceffb3d5a18c
Saish Ramdas Natekar (Digital) <SA20075541@wipro.com>
2019-04-26 13:10:12 +0530

    reverted dtasource

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,8 +18,8 @@ import java.util.TimeZone;
 @SpringBootApplication
 public class PalTrackerApplication {
 
-    @Value("spring.datasource.url")
-    DriverManagerDataSource dataSource;
+    @Autowired
+    DataSource dataSource;
 
     public static void main (String args[]){
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
